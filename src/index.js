a8c05a60658acd2d0f8987e6918bc5c73f2a79e8
mousehacks <77026371+mousehacks@users.noreply.github.com>
2021-02-07 22:17:45 +0100

    final commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
